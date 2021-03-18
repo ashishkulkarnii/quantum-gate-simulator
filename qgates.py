250f6267ac50b810d98d12957adc5333b6f71dc2
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2021-03-19 00:51:45 +0530

    Update qgates.py
    added support for controlled gates, and collapsing of probability amplitudes upon usage of gates requiring measurement

--- qgates.py
+++ qgates.py
@@ -5,13 +5,19 @@ from random import random
 from os import system, name 
 
 
+#some auxiliary functions:
+
+
 def clear(): 
     if name == 'nt': 
         _ = system('cls') 
 
 
-def report(q):
+def reportq1(q):
     print("The vector representation of q1 is: {}\nThe chances of |0> are {}% and the chances of |1> are {}%.\n".format(*q))
+
+def reportq2(q):
+    print("The vector representation of q2 is: {}\nThe chances of |0> are {}% and the chances of |1> are {}%.\n".format(*q))
 
 
 def ket(n):
@@ -23,6 +29,11 @@ def ket(n):
         chanceket0 = 0
         chanceket1 = 100
         return [[[0], [1]], chanceket0, chanceket1]
+
+
+def measurementStatement(q1, q2):
+    print("\nThe measurement of q1 is: ",measureQubit(q1))
+    print("The measurement of q2 is: ",measureQubit(q2))
 
 
 #all arguments are lists of the form [<vetor representation of qubit>, <chance of ket(0)>, <chance of ket(1)>]
@@ -43,7 +54,6 @@ def pauliX(k):
 
 def pauliY(k):
     output = np.dot([[0, complex(0,-1)],[complex(0,1), 0]], k[0]).tolist()
-    print(output)
     chanceket0 = abs(output[0][0] * output[0][0] * 100)
     chanceket1 = abs(output[1][0] * output[1][0] * 100)
     return [output, chanceket0.real, chanceket1.real]
@@ -51,44 +61,74 @@ def pauliY(k):
 
 def pauliZ(k):
     z = [[1, 0], [0, -1]]
-    output = np.dot(z, k[0]).tolist()
-    print(output)   
+    output = np.dot(z, k[0]).tolist()  
     chanceket0 = abs(output[0][0] * output[0][0] * 100)
     chanceket1 = abs(output[1][0] * output[1][0] * 100)
     return [output, chanceket0.real, chanceket1.real]
 
 
-def cx(control, target):
-    if control[2] != 0:
-        print("{}% chance of {}".format(control[1], target))
-        print("{}% chance of {}".format(control[2], pauliX(target)))
+def cx1(q2, q1):
+    if measureQubit(q2) == "|0>":
+        print("The measurement of q1 is:",measureQubit(q1))
+        print("The measurement of q2 is: |0>")
     else:
-        output = target
-        return output
+        print("The measurement of q1 is:",measureQubit(pauliX(q1)))
+        print("The measurement of q2 is: |1>")
+    quit()
 
-
-def cy(control, target):
-    if control[2] != 0:
-        print("{}% chance of {}".format(control[1], target))
-        print("{}% chance of {}".format(control[2], pauliY(target)))
+def cx2(q1, q2):
+    if measureQubit(control) == "|0>":
+        print("The measurement of q1 is: |0>")
+        print("The measurement of q2 is:",measureQubit(q2))
     else:
-        output = target
-        return output
+        print("\nThe measurement of q1 is: |1>")
+        print("The measurement of q2 is:",measureQubit(pauliX(q2)))
+    quit()
 
 
-def cz(control, target):
-    if control[2] != 0:
-        print("{}% chance of {}".format(control[1], target))
-        print("{}% chance of {}".format(control[2], pauliZ(target)))
+def cy1(q2, q1):
+    if measureQubit(q2) == "|0>":
+        print("The measurement of q1 is:",measureQubit(q1))
+        print("The measurement of q2 is: |0>")
     else:
-        output = target
-        return output
+        print("The measurement of q1 is:",measureQubit(pauliY(q1)))
+        print("The measurement of q2 is: |1>")
+    quit()
+
+def cy2(q1, q2):
+    if measureQubit(q1) == "|0>":
+        print("The measurement of q1 is: |0>")
+        print("The measurement of q2 is:",measureQubit(q2))
+    else:
+        print("The measurement of q1 is: |1>")
+        print("The measurement of q2 is:",measureQubit(pauliY(q2)))
+    quit()    
+
+
+def cz1(q2, q1):
+    if measureQubit(q2) == "|0>":
+        print("The measurement of q1 is:",measureQubit(q1))
+        print("The measurement of q2 is: |0>")
+    else:
+        print("The measurement of q1 is:",measureQubit(pauliZ(q1)))
+        print("The measurement of q2 is: |1>")
+    quit()
+    
+def cz2(q1, q2):
+    if measureQubit(q1) == "|0>":
+        print("The measurement of q1 is: |0>")
+        print("The measurement of q2 is:",measureQubit(q2))
+    else:
+        print("The measurement of q1 is: |1>")
+        print("The measurement of q2 is:",measureQubit(pauliZ(q2)))
+    quit()
 
 
 def measureQubit(k):
     m = 100 * random()
     p0 = k[1]
-    if m < p0:
+    if m < p0 and m != 0:
         return "|0>"
     else:
         return "|1>"
+ 
